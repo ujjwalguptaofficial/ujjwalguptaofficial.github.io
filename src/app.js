ce3ae34a6a125f543551588be86cd04b28b773be
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2019-07-03 12:15:06 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+window.jQuery = window.$ = require('jquery');
+import "jquery.easing";
+// require('jquery.easing')($);
+import 'bootstrap/scss/bootstrap';
+import 'bootstrap/dist/js/bootstrap.min';
+import "./scss/resume";
+
+export class App {
+    init() {
+        // Smooth scrolling using jQuery easing
+        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top)
+                    }, 1000, "easeInOutExpo");
+                    return false;
+                }
+            }
+        });
+
+        // Closes responsive menu when a scroll trigger link is clicked
+        $('.js-scroll-trigger').click(function () {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        // Activate scrollspy to add active class to navbar items on scroll
+        $('body').scrollspy({
+            target: '#sideNav'
+        });
+    }
+}
+
+new App().init();
---
+++ src/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+console.log('env', process.env.NODE_ENV);
+const folder = process.env.NODE_ENV == "production" ? "docs" : "dist";
+module.exports = {
+    entry: './src/app.js',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: "css-loader" // translates CSS into CommonJS
+            }]
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.scss']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, `../${folder}/`)
+    },
+    plugins: [
+        new CopyPlugin([{
+            from: './src/img/',
+            to: 'img/'
+        }]),
+        new HtmlWebpackPlugin({
+            cache: true,
+            hash: true,
+            template: 'src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true
+            }
+        })
+    ]
+};
